1f7cdb9fa40b612009b3bd08d700145799413ed8
flavia-kihahu <kihahuflavia@gmail.com>
2026-06-20 18:58:32 +0300

    functions

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
+function calculateTax(amount) {
+    let taxValue = amount * 0.1;
+    return taxValue;
+}
 
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
+
+function findMaximum(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
+
+function isPalindrome(word) {
+    if (word === word.split("").reverse().join("")) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    const price = originalPrice - discountPercentage;
+    return price;
+}
+let discountedPrice = calculateDiscountedPrice(100, 20);
+console.log(discountedPrice);
 
 
 
